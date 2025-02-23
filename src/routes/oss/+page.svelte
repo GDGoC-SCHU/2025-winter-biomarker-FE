264d4a33bc20cfe04dc3240a7cf9dd6c40bf1e1d
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-02-24 05:05:17 +0900

    (feat) add open source license

---
+++ src/routes/oss/+page.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+
+</script>
+
+<main class="flex justify-center items-center m-4">
+    <section>
+        <h1 class="text-3xl">오픈소스 라이선스</h1>
+        <p class="text-base">
+            본 프로젝트에 사용한 오픈소스 라이선스를 확인하실 수 있어요.<br>
+            소스코드는
+            <a href="https://github.com/GDGoC-SCHU/2025-winter-biomarker-FE" target="_blank" class="link">
+                여기
+            </a>에서 확인하실 수 있어요.
+        </p>
+        <ul class="list">
+            <li class="list-row">
+                <div class="list-col-grow">
+                    <h2 class="text-lg flex gap-2 items-center">
+                        <span class="badge badge-info shrink-0">글꼴</span>
+                        <span>Material Symbols Rounded</span>
+                    </h2>
+                    <p>© 2025 Google. Apache 2.0 License</p>
+                </div>
+                <a href="https://fonts.google.com/icons" target="_blank" class="btn btn-sqaure btn-ghost">
+                    <span class="material-symbols-rounded">open_in_new</span>
+                </a>
+            </li>
+            <li class="list-row">
+                <div class="list-col-grow">
+                    <h2 class="text-lg flex gap-2 items-center">
+                        <span class="badge badge-info shrink-0">글꼴</span>
+                        <span>고운돋움</span>
+                    </h2>
+                    <p>© 2021 류양희. SIL Open Font License Version 1.1</p>
+                </div>
+                <a href="https://github.com/yangheeryu/Gowun-Dodum" target="_blank" class="btn btn-sqaure btn-ghost">
+                    <span class="material-symbols-rounded">open_in_new</span>
+                </a>
+            </li>
+            <li class="list-row">
+                <div class="list-col-grow">
+                    <h2 class="text-lg flex gap-2 items-center">
+                        <span class="badge badge-success shrink-0">UI</span>
+                        <span>daisyUI</span>
+                    </h2>
+                    <p>© 2025 Pouya Saadeghi, daisyUI contributors. MIT License</p>
+                </div>
+                <a href="https://v5.daisyui.com/" target="_blank" class="btn btn-sqaure btn-ghost">
+                    <span class="material-symbols-rounded">open_in_new</span>
+                </a>
+            </li>
+            <li class="list-row">
+                <div class="list-col-grow">
+                    <h2 class="text-lg flex gap-2 items-center">
+                        <span class="badge badge-success shrink-0">UI</span>
+                        <span>Tailwind CSS</span>
+                    </h2>
+                    <p>© 2025 Tailwind Labs Inc. MIT License</p>
+                </div>
+                <a href="https://tailwindcss.com/" target="_blank" class="btn btn-sqaure btn-ghost">
+                    <span class="material-symbols-rounded">open_in_new</span>
+                </a>
+            </li>
+            <li class="list-row">
+                <div class="list-col-grow">
+                    <h2 class="text-lg flex gap-2 items-center">
+                        <span class="badge badge-soft badge-primary shrink-0">프레임워크</span>
+                        <span>SvelteKit</span>
+                    </h2>
+                    <p>© 2024 Svelte contributors. MIT License</p>
+                </div>
+                <a href="https://svelte.dev/" target="_blank" class="btn btn-sqaure btn-ghost">
+                    <span class="material-symbols-rounded">open_in_new</span>
+                </a>
+            </li>
+        </ul>
+    </section>
+</main>
