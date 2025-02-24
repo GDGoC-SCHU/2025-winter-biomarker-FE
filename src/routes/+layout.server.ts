c087cd3f29e90a1b4ace058a1376c9ca46933528
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-02-24 22:38:51 +0900

    (fix) remove deprecated cookie

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,16 +3,13 @@ import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
     const session = cookies.get("userId");
-    const email = cookies.get("realId");
     const name = cookies.get("name");
 
-    if (session && email && name) {
+    if (session && name) {
         return {
-            email: email.toString(),
             name: name?.toString()
         }
     } else {
         cookies.delete("userId", { path: "/" });
-        cookies.delete("realId", { path: "/" });
     }
 }
