3ea5e2442d70508f2a3ff7978b993dc841e550b1
paperbox <7126454+lego37yoon@users.noreply.github.com>
2025-02-24 23:29:56 +0900

    (feat) update info with real server

--- src/routes/update/+page.server.ts
+++ src/routes/update/+page.server.ts
@@ -1,10 +1,39 @@
-import { redirect } from "@sveltejs/kit";
-import type { Actions, PageServerLoad } from "./$types";
+import type { Actions } from "./$types";
+import { env } from "$env/dynamic/private";
 
-interface GenderType { value: string, text: string };
+export const actions = {
+    default: async ({ cookies, request }) => {
+        const data = await request.formData();
+        const userId = cookies.get("userId");
 
-export const load: PageServerLoad = async ({ cookies }) => {
-    const gender = cookies.get("gender");    
+        const body = {
+            "height": data.get("height"),
+            "weight": data.get("weight"),
+            "body_fat_percentage": data.get("body_fat_percentage"),
+            "skeletal_muscle_mass": data.get("skeletal_muscle_mass"),
+            "bmr": data.get("bmr"),
+            "body_fat_mass": data.get("body_fat_mass"),
+            "goal": data.get("goal")
+        }
+        
+        if (userId) {
+            const req = await fetch(`${env.BACKEND_ADDRESS}/user/health/add/${userId}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            });
 
-    return { gender: gender };
-}
+            if (req.ok) {
+                return {
+                    success: true
+                }
+            }
+        }
+        
+        return {
+            success: false
+        }
+    }
+} satisfies Actions
